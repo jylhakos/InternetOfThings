9c944f037fa9741130fa377dbd6d1778d48ab4f6
laptop <juha.jylhakoski@gmail.com>
2025-06-07 11:41:50 +0300

    Basics of Golang

---
+++ Frameworks/Backend/Basics/go.mod
@@ -0,0 +1,3 @@
+module github.com/jylhakos/InternetOfThings/blob/main/Frameworks/Backend/Basics
+
+go 1.24.4
---
+++ Frameworks/Backend/Basics/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var text string = "Go"
+	fmt.Println(text)
+}
